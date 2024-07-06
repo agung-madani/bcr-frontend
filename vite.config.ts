36b5315b7e7991dd9e07ba599a37498ab5b1f438
20106050083 <agungrashif009@gmail.com>
2024-07-06 13:15:40 +0700

    fixing dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Specify your app's base URL if necessary
-  build: {
-    outDir: "build", // Specify your output directory
-    emptyOutDir: true, // Clear output directory before building
-    manifest: true, // Generate manifest.json for PWA
-    rollupOptions: {
-      // Add any rollup options here
-    },
-  },
+  base: "/",
 });
